2b090065564f80122c800510bde8b891ab001038
Byungho Lee <blee03291@gmail.com>
2020-11-19 19:10:53 +0900

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,57 @@
+variable "location" {
+    type = string
+    default  = "westus2"
+}
+
+variable "admin_username" {
+    type = string
+    default = "azuretest"
+    description = "Administrator user name for virtual machine"
+
+}
+
+variable "admin_password" {
+    type = string
+    default = "Gtgsc1314"
+    description = "Password must meet Azure complexity requirements"
+}
+
+
+variable "prefix" {
+  type    = string
+  default = "my"
+}
+
+variable "tags" {
+  type = map
+
+  default = {
+    Environment = "Terraform GS"
+    Dept        = "Engineering"
+  }
+}
+
+variable "sku" {
+  default = {
+    westus2 = "16.04-LTS"
+    eastus  = "18.04-LTS"
+  }
+}
+
+variable "client_id" {
+  type    = string
+}
+
+variable "subscription_id" {
+  type    = string
+}
+
+variable "client_secret" {
+  type    = string
+}
+
+variable "tenant_id" {
+  type    = string
+}
+
+
